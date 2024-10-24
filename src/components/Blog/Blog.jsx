4e277eb25ec3e822d6deff2da4a228a0f811a6c7
mohsyn007 <mohsinalam1141@gmail.com>
2024-10-24 14:28:24 +0600

    Handle Bookmark

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,40 @@
 import PropTypes from 'prop-types';
-const Blog = (blog) => {
-    console.log(blog);
+import { FaBookmark } from "react-icons/fa";
+
+const Blog = ({blog,handleAddToBookmark}) => {
+    const {title,cover,author,author_img,reading_time,posted_date,hashtag}=blog;
     return (
         <div>
-            
+           
+           <img className='w-full mb-8' src={cover} alt={`cover picture of the ${title}`} />
+           <div className='flex justify-between mb-4'>
+            <div className='flex'>
+                <img className='w-14' src={author_img} alt="" />
+                <div className='ml-6'>
+                    <h3 className='text-2xl'>{author}</h3>
+                    <p>{posted_date}</p>
+                </div>
+
+            </div>
+            <div>
+                <span>{reading_time} mins read</span>
+                <button onClick={handleAddToBookmark} className='ml-2 text-2xl text-red-500'><FaBookmark></FaBookmark></button>
+
+            </div>
+           </div>
+           <h3 className='text-4xl'>{title}</h3>
+           <p>
+            {
+                hashtag.map((hash,idx) => <span key={idx}><a href=''>#{hash} </a> </span>)
+            }
+    
+           </p>
         </div>
     );
 };
+
+
+
 Blog.propTypes={
     blog:PropTypes.object.isRequired
 }
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
-const Blogs = () => {
+const Blogs = (handleAddToBookmark) => {
     const [blogs,setBlogs]=useState([]);
     useEffect(()=>{
         fetch('blogs.json')
@@ -11,7 +12,10 @@ const Blogs = () => {
 
     return (
         <div className="md:w-2/3">
-           <h3 className="text-4xl">Blogs</h3>
+           <h3 className="text-4xl">Blogs:{blogs.length}</h3>
+           {
+            blogs.map(blog=><Blog key={blog.id} blog={blog} handleAddToBookmark={handleAddToBookmark}></Blog>)
+           }
         </div>
     );
 };
